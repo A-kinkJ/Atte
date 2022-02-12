21474b59b6395cb848666ebe3654133ded2ad49c
A-kikJ <patliott_h_apollon@yahoo.co.jp>
2022-02-12 15:39:28 +0900

    13

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -85,7 +85,7 @@ $title = '日付一覧'
       </form>
     </li>
     <li>
-      <form action="/userist" method="GET" name="userList">
+      <form action="/userlist" method="GET" name="userList">
         @csrf
         <a href="javascript:userList.submit()">ユーザー一覧</a>
       </form>
